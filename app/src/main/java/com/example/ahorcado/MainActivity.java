7615e377b4282b3c178047c319eb9ef563069c47
alejandroserranol <alejandroserranol@hotmail.com>
2020-03-13 10:22:52 +0100

    Con la fin de partida

--- app/src/main/java/com/example/ahorcado/MainActivity.java
+++ app/src/main/java/com/example/ahorcado/MainActivity.java
@@ -8,9 +8,12 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
-    String palabreOculta = "CETYS";
+    String palabraOculta = "CETYS";
+    boolean partidaTerminada = false; //indica si la partida ha terminado
     int numeroDeFallos = 0;
 
     @Override
@@ -23,10 +26,32 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    protected void onStart(){
+        super.onStart();
+        palabraOculta = eligePalabra();
+        String barras = "";
+        for (int i=0; i<palabraOculta.length(); i++){
+            barras += "_";
+        }
+
+        ((TextView) findViewById(R.id.palabraConGuiones)).setText(barras);
+
+    }
+
+    private String eligePalabra() {
+        String[] listaPalabras = {"HOLA", "VLADIKAKA", "CETYS", "BORREGUITO", "BABYYODA", "MANDO"};
+        Random aleatorio = new Random(); //Variable aleatoria va a elegir una palabra al azahar
+        int posicion = aleatorio.nextInt(listaPalabras.length);
+        return listaPalabras[posicion].toUpperCase();
+    }
+
     public void botonPulsado(View vista){
-        Button boton = findViewById(vista.getId());
-        boton.setVisibility(View.INVISIBLE);
-        chequeaLetra(boton.getText().toString());
+        if(!partidaTerminada) {
+            Button boton = findViewById(vista.getId());
+            boton.setVisibility(View.INVISIBLE);
+            chequeaLetra(boton.getText().toString());
+        }
     }
 
     private void chequeaLetra(String letra){
@@ -37,18 +62,25 @@ public class MainActivity extends AppCompatActivity {
 
         boolean acierto = false;
 
-        for (int i=0; i<palabreOculta.length();i++){
-            if(palabreOculta.charAt(i) == letra.charAt(0)){
-                //quita el guion bajo la ley correspondiente
-                palabraConGuiones = palabraConGuiones.substring(0, 2*i)
-                        + letra
-                        + palabraConGuiones.substring(2*i+1);
-                acierto = true;
+        if (palabraOculta.contains(letra)) {
+            for (int i = 0; i < palabraOculta.length(); i++) {
+                if (palabraOculta.charAt(i) == letra.charAt(0)) {
+                    //quita el guion bajo la ley correspondiente
+                    palabraConGuiones = palabraConGuiones.substring(0, 2 * i)
+                            + letra
+                            + palabraConGuiones.substring(2 * i + 1);
+                    acierto = true;
+                }
             }
-        }
 
-        if(!palabraConGuiones.contains("_")){
-            imagenAhorcado.setImageResource(R.drawable.acertastetodo);
+            if (!palabraConGuiones.contains("_")) {
+                imagenAhorcado.setImageResource(R.drawable.acertastetodo);
+            }
+        } else {
+            numeroDeFallos++;
+            if (numeroDeFallos == 6) {
+                partidaTerminada = true;
+            }
         }
 
         textoConGuiones.setText(palabraConGuiones);
